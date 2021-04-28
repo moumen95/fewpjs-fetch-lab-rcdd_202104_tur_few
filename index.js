46f09fc2f57d43d7b7fbc29b9dae39a08c3af827
Moumen Hallak <moumen95@gmail.com>
2021-04-28 22:02:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(function(response){
-  return response.json()
-  })
-  .then(function(json){
-  })
-  return renderBooks(books)
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
